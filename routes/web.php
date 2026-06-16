905eb152688851d5ce855c9524083b165393ac79
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-15 21:21:44 -0300

    email test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Mail\Message;
+use Throwable;
 
-Route::get('/', function () {
-    echo "RH MANGNT";
+Route::get('/email', function () {
+  
+        Mail::raw('Mensagem de teste de RH MANGNT', function (Message $message) {
+            $message->to('teste@gmail.com')
+                ->subject('Teste de Email')
+                ->from('rh@rh_mangnt.com');
+        });
+
+        echo 'Email enviado com sucesso!';
+ 
 });
