6e56285d0325fa9a5121cc9d2a5991cfd9a6c401
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-15 21:40:44 -0300

    create db and make a migration

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Mail\Message;
-use Throwable;
+
 
 Route::get('/email', function () {
   
