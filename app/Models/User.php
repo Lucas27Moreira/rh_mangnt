0f88c7c8562b96738682b46b251ac3d18d7e34bd
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-16 21:23:14 -0300

    create the relation models

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Model
 {
-    //
+    use HasFactory;
+
+    public function userDetail()
+    {
+        return $this->hasOne(UserDetail::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
