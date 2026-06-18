ca7f792d887cfe609ae8b20c729858729f254e31
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-17 22:00:09 -0300

    init the page login

---
+++ app/View/Components/LayoutGuest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class LaoutGuest extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($pageTitle = null)
+    {
+        $this->pageTitle = $pageTitle;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.laout-guest');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Mail\Message;
-use App\Models\User;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/email', function () {
-    try {
-        Mail::raw('Mensagem de teste de RH MANGNT', function (Message $message) {
-            $message->to('teste@gmail.com')
-                ->subject('Teste de Email')
-                ->from('rh@rh_mangnt.com');
-        });
-
-        return 'Email enviado com sucesso!';
-    } catch (\Exception $e) {
-        Log::error('Erro ao enviar email: ' . $e->getMessage());
-        return 'Erro ao enviar email. Verifique os logs.';
-    }
-});
-
-Route::get('/admin', function () {
-    $admin = User::with(['userDetail', 'department'])->find(1);
-    
-    if (!$admin) {
-        return 'Administrador não encontrado.';
-    }
-
-    return view('admin', ['admin' => $admin]);
-});
+Route::view('/login', 'auth.login');
