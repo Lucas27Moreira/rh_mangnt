fc99b87449ea884a278223ffadcbf40e0599c011
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-21 20:22:43 -0300

    add informations home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,5 @@
 <x-layout-app pageTitle="Home">
-    <h1 class="text-center">DENTRO DO HOME</h1>
+    <h1 class="text-center my-5">DENTRO DO HOME</h1>
 
-    @php 
-    dump(auth()->user());
-    @endphp
+   
 </x-layout-app>
