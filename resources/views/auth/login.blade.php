4e1bd5f44bd11eaa4184fcd0a9f789a4fa0779fb
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-23 21:40:32 -0300

    put a field if the status is ok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,6 +36,11 @@
                     </div>
 
                 </form>
+                @if (session('status'))
+                    <div class="alert alert-success mt-3">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
             </div>
 
