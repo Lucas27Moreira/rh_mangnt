838ce510f9f6702ecfb9eb772f395fc2b486b293
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-27 20:34:18 -0300

    update password

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,4 +12,28 @@ class ProfileController extends Controller
     {
        return view('user.profile');
     }
+
+    public function updatePassword(Request $request)
+    {
+        // form validation
+        $request->validate([
+            'current_password' => 'required|min:8|max:16',
+            'new_password' => 'required|min:8|max:16|different:current_password',
+            'new_password_confitmation' => 'required|same:new_password',
+        ]);
+
+        $user = auth()->user();
+
+        // check if the current password is correct
+        if(!password_verify($request->current_password, $user->password))
+        {
+            return redirect()->back()->width('error','current password is incorrect');
+        }
+
+        //updatepassword in database
+        $user->password = bcrypt($request->new_password);
+        $user->save();
+
+        return redirect()->back()->width('success','Password updated successfully.');
+    }
 }
