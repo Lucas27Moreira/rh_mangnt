5e54e65ca773feb949e68c6ed94b497a4fe8ebf1
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-06-27 20:42:55 -0300

    fix method updatePassword

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -19,7 +19,7 @@ class ProfileController extends Controller
         $request->validate([
             'current_password' => 'required|min:8|max:16',
             'new_password' => 'required|min:8|max:16|different:current_password',
-            'new_password_confitmation' => 'required|same:new_password',
+            'new_password_confirmation' => 'required|same:new_password',
         ]);
 
         $user = auth()->user();
@@ -27,13 +27,13 @@ class ProfileController extends Controller
         // check if the current password is correct
         if(!password_verify($request->current_password, $user->password))
         {
-            return redirect()->back()->width('error','current password is incorrect');
+            return redirect()->back()->with('error','current password is incorrect');
         }
 
         //updatepassword in database
         $user->password = bcrypt($request->new_password);
         $user->save();
 
-        return redirect()->back()->width('success','Password updated successfully.');
+        return redirect()->back()->with('success','Password updated successfully.');
     }
 }
