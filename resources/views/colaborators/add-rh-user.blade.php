d4fe549d531ced4bd07b5b82f42bf8903559b887
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-07-06 21:05:04 -0300

    form add rh user

--- resources/views/colaborators/add-rh-user.blade.php
+++ resources/views/colaborators/add-rh-user.blade.php
@@ -5,7 +5,7 @@
                 <div class="col-4">
                     <h3>New Human Resources Colaborator</h3>
                     <hr>
-                    <form action="#" method="post">
+                    <form action="{{ route('colaborators.create-colaborator') }}" method="post">
                         @csrf
                         <div class="mb-3">
                             <label for="name" class="form-label">Name</label>
@@ -21,14 +21,26 @@
                                 <div class="text-danger">{{ $message }}</div>
                             @enderror
                         </div>
-                           <div class="mb-3">
-                          <label for="select_department" class="form-label">Department</label>
-                          <select class="form-select" id="department" name="department" required>
-                            <option value="">Select a department</option>
-                            @foreach ($departments as $department)
-                              <option value="{{ $department->id }}">{{ $department->name }}</option>
-                            @endforeach
-                          </select>
+
+                        <div class="mb-3">
+                            <div class="d-flex">
+
+                                <div class="flex-grow-1 pe-3">
+                                    <label for="select_department" class="form-label">Department</label>
+                                    <select class="form-select" id="department" name="department" required>
+                                    <option value="">Select a department</option>
+                                    @foreach ($departments as $department)
+                                        <option value="{{ $department->id }}">{{ $department->name }}</option>
+                                    @endforeach
+                                        </select>
+                                    @error('select_department')
+                                <div class="text-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div>
+                                    <a href="{{ route('departments.new-department') }}" class="btn btn-outline-primary"><i class="fas fa-plus"></i></a>
+                                </div>
+                            </div>
                         </div>
                         <p class="mb-3">Profile: <strong>Human Resources</strong></p>
                         <div class="mb-3">
