d84757b1f5879417c52203fe4f079b21b6ce82e5
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-07-16 22:23:08 -0300

    fix delete

--- resources/views/department/delete-department-confirm.blade.php
+++ resources/views/department/delete-department-confirm.blade.php
@@ -10,7 +10,10 @@
         <div class="text-center">
             <h3 class="my-5">{{ $department->name }}</h3>
             <a href="{{ route('departments') }}" class="btn btn-secondary px-5">No</a>
-            <a href="{{ route('departments.delete-department-confirm', ['id' => $department->id]) }}" class="btn btn-danger px-5">Yes</a>
+            <form action="{{ route('departments.delete-department-confirm', ['id' => $department->id]) }}" method="POST" class="d-inline">
+                @csrf
+                <button type="submit" class="btn btn-danger px-5">Yes</button>
+            </form>
         </div>
         
     </div>
