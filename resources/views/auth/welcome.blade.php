644b5e1c328dca41d8e9fda5289fde32bddb5ed1
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-07-22 21:03:51 -0300

    welcon page

---
+++ resources/views/auth/welcome.blade.php
@@ -0,0 +1,16 @@
+<x-layout-guest pageTitle="Welcome">
+    <div class="row justify-content-center">
+        <div class="col">
+            {{-- logo --}}
+            <div class="text-center mb-5">
+                <img src="{{ asset('assets/images/logo.png') }}" alt="Logo" width="200px">
+            </div>
+            {{-- welcome message --}}
+            <div class="card p-5 text-center">
+                <h1 class="text-center mb-4">Welcome {{$user->name}}</h1>
+                <p class="text-center">You have successfully registered!</p>
+                <p class="text-center">You can now <a href="{{ route('login') }}" class="btn btn-primary">login</a> to your account.</p>
+            </div>
+        </div>
+    </div>
+</x-layout-guest>
