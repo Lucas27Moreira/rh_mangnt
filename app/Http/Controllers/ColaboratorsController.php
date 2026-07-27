4c4c143fc6f7ab7d24fd15a44bec70e4b9b9a34b
Lucas Moreira <Lucas27Moreira@users.noreply.github.com>
2026-07-26 22:42:05 -0300

    showdetails

--- app/Http/Controllers/ColaboratorsController.php
+++ app/Http/Controllers/ColaboratorsController.php
@@ -16,4 +16,16 @@ class ColaboratorsController extends Controller
           $colaborators = User::with('userDetail', 'department')->where('role', '<>', 'admin')->get();
           return view('colaborators.admin-all-colaborators')->with('colaborators', $colaborators);
     }
+
+    public function showDetails($id)
+    {
+        Auth::user()->can('admin', 'rh')?: abort(403, 'You are not authorized to access this page');
+
+        //check if id is the same as the logged in user id
+        if (Auth::user()->id == $id) {
+            return redirect()->route('home');
+        }
+        $colaborator = User::with('userDetail', 'department')->where('id', $id)->first();
+        return view('colaborators.show-details')->with('colaborator', $colaborator);
+    }
 }
